6f6409c0eeb6ed92ea9e3e36ffe8f439ebdd422f
Ifu Aniemeka <ianiemeka@gmail.com>
2014-03-18 17:49:06 -0500

    21 passes, 30 failures

--- app/objects.js
+++ app/objects.js
@@ -3,7 +3,8 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      obj.sayIt = fn;
+      return obj.sayIt();
     },
 
     alterObjects : function(constructor, greeting) {
