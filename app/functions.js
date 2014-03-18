c000ce3e8070bb688a3ea8c56e717df1679a864b
Ifu Aniemeka <ianiemeka@gmail.com>
2014-03-18 17:27:56 -0500

    19 passes, 32 failures

--- app/functions.js
+++ app/functions.js
@@ -3,11 +3,12 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn(arr[0], arr[1], arr[2]);
     },
 
     speak : function(fn, obj) {
-
+         obj.func = fn;
+         return obj.func();
     },
 
     functionFunction : function(str) {
